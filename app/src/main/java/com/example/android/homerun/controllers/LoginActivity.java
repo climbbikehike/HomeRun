e1c938c3da19a93eaa0650b07afd7fadbd72020f
Animesh Fatehpuria <animesh_fatehpuria@outlook.com>
2018-02-13 19:19:08 -0500

    Added activity to go to blank page upon correct login

--- app/src/main/java/com/example/android/homerun/controllers/LoginActivity.java
+++ app/src/main/java/com/example/android/homerun/controllers/LoginActivity.java
@@ -8,7 +8,9 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
+import android.app.Activity;
 
+import android.content.Intent;
 import android.content.CursorLoader;
 import android.content.Loader;
 import android.database.Cursor;
@@ -187,7 +189,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
             showProgress(true);
-            mAuthTask = new UserLoginTask(email, password);
+            mAuthTask = new UserLoginTask(email, password, this);
             mAuthTask.execute((Void) null);
         }
     }
@@ -302,10 +304,12 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         private final String mEmail;
         private final String mPassword;
+        private Activity mActivity;
 
-        UserLoginTask(String email, String password) {
+        UserLoginTask(String email, String password, Activity activity) {
             mEmail = email;
             mPassword = password;
+            mActivity = activity;
         }
 
         @Override
@@ -326,7 +330,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                     return pieces[1].equals(mPassword);
                 }
             }
-
             return false;
             // TODO: register the new account here.
             // return true;
@@ -338,8 +341,12 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
-                finish();
+                // Go to a blank page if valid username/password.
+                // TODO: Add logout button here.
+                mActivity.startActivity(new Intent(mActivity, LoggedInActivity.class));
             } else {
+                // Right now, it shows "incorrect password".
+                // Ideally, create an alert box.
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
             }
