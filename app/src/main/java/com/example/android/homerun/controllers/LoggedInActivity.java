443a230d7bc097183e29d952b39d88d4344e6e11
jjacob35 <jeffrey.jacob@gatech.edu>
2018-02-13 19:50:14 -0500

    Log Out Button
    Log Out Returns to start page

--- app/src/main/java/com/example/android/homerun/controllers/LoggedInActivity.java
+++ app/src/main/java/com/example/android/homerun/controllers/LoggedInActivity.java
@@ -1,8 +1,13 @@
 package com.example.android.homerun.controllers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.android.homerun.R;
+
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class LoggedInActivity extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class LoggedInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_in);
+        Button logout_button = (Button) findViewById(R.id.logout_button);
+        logout_button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                returntoStart();
+            }
+        });
+    }
+
+    private void returntoStart() {
+        startActivity(new Intent(LoggedInActivity.this, LoginActivity.class));
     }
 }
