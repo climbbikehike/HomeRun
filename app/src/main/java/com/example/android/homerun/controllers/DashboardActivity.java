f67c1913ca075bba3ac79379fd1756d42cc0f216
Prabhav Chawla <prabhav_chawla@yahoo.co.in>
2018-02-27 22:27:59 -0500

    Fix and refactor a lot of stuff because I have ADHD

--- app/src/main/java/com/example/android/homerun/controllers/DashboardActivity.java
+++ app/src/main/java/com/example/android/homerun/controllers/DashboardActivity.java
@@ -2,6 +2,7 @@ package com.example.android.homerun.controllers;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.android.homerun.R;
@@ -17,32 +18,24 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.Window;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.io.InputStream;
 import java.util.ArrayList;
 
 public class DashboardActivity extends AppCompatActivity {
 
-    private Button logout_button;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Window w = getWindow();
-        w.setTitle("Dashboard");
-        setContentView(R.layout.activity_logged_in);
+        setContentView(R.layout.activity_dashboard);
 
-        final ListView shelter_list = (ListView)findViewById(R.id.shelter_list);
-        TextView emptyText = (TextView)findViewById(android.R.id.empty);
-        shelter_list.setEmptyView(emptyText);
-
+        setTitle("Shelters");
 
         DatabaseReference shelterRef = FirebaseDatabase.getInstance().getReference()
                 .child(FirebaseWrapper.DATABASE_SHELTERS);
@@ -59,25 +52,22 @@ public class DashboardActivity extends AppCompatActivity {
                 }
 
                 ArrayAdapter<Shelter> shelterAdapter = new ShelterAdapter(DashboardActivity.this, shelterList);
-                shelter_list.setAdapter(shelterAdapter);
 
-                /* ListView listView = (ListView) findViewById(R.id.list);
+                ListView listView = (ListView) findViewById(R.id.shelter_list);
                 assert listView != null;
                 listView.setAdapter(shelterAdapter);
-                listView.setOnClickListener(new OnClickListener() {
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+                {
                     @Override
-                    public void onClick(View view) {
-
+                    public void onItemClick(AdapterView<?> adapter, View v, int position,
+                                            long arg3)
+                    {
+                        Shelter shelter = (Shelter) adapter.getItemAtPosition(position);
+                        Intent intent = new Intent(DashboardActivity.this, ShelterDetailActivity.class);
+                        intent.putExtra("ShelterData", shelter);
+                        startActivity(intent);
                     }
-                    /*
-                    @Override
-                    public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-                                            long arg3) {
-                        // TODO Auto-generated method stub
-                        // Log.d("############","Items " +  MoreItems[arg2] );
-                    }
-
-                }); */
+                });
             }
 
             @Override
@@ -102,5 +92,24 @@ public class DashboardActivity extends AppCompatActivity {
                     }
                 });
         dlgAlert.create().show();
+    }
+
+    // create an action bar button
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.custom_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // handle button activities
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.logout_action) {
+            FirebaseAuth.getInstance().signOut();
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/android/homerun/view/ShelterAdapter.java
+++ app/src/main/java/com/example/android/homerun/view/ShelterAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import com.example.android.homerun.R;
 import com.example.android.homerun.model.Shelter;
@@ -25,34 +26,19 @@ public class ShelterAdapter extends ArrayAdapter<Shelter> {
     @NonNull
     @Override
     public View getView(int position, View convertView, @NonNull ViewGroup parent) {
-        // Note that the parent here is ListView in our activity_numbers.xml file
-        // Check if the existing view is being reused, otherwise inflate the view
         View listItemView = convertView;
         if(listItemView == null) {
-            // listItemView = LayoutInflater.from(getContext()).inflate(R.layout.shelter_layout, parent, false);
-            // listItemView is the LinearLayout with the two TextView children in word_pair_layout.xmlxml
-            /**
-             * Inflate a new view hierarchy from the specified xml resource.
-             * resource	int: ID for an XML layout resource to load (e.g., R.layout.main_page)
-             * root	ViewGroup: Optional view to be the parent of the generated hierarchy
-             (if attachToRoot is true), or else simply an object that provides
-             a set of LayoutParams values for root of the returned hierarchy (if attachToRoot is false.)
-             attachToRoot boolean: Whether the inflated hierarchy should be attached to the root parameter?
-             If false, root is only used to create the correct subclass of LayoutParams
-             for the root view in the XML. We don't want to attach it to
-             */
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.shelter_list_item, parent, false);
         }
 
         Shelter shelter = getItem(position);
 
-        /* TextView englishWord = (TextView) listItemView.findViewById(R.id.english_word);
-        TextView translatedWord = (TextView) listItemView.findViewById(R.id.translated_word);
-        ImageView imageView = (ImageView) listItemView.findViewById(R.id.img);*/
+        TextView name = (TextView) listItemView.findViewById(R.id.shelter_name);
+        TextView capcity = (TextView) listItemView.findViewById(R.id.shelter_capacity);
 
         assert shelter != null;
-        /* englishWord.setText(currData.getEnglishWord());
-        translatedWord.setText(currData.getTranslatedWord());*/
-
+        name.setText(shelter.getName());
+        capcity.setText("Capacity: " + shelter.getCapacity());
 
         return listItemView;
     }
