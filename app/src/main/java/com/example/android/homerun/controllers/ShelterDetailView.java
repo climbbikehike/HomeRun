17ffa3dc99c6de851645dc1528ba31416e0dd37c
Animesh Fatehpuria <animesh_fatehpuria@outlook.com>
2018-02-27 19:54:34 -0500

    Added backend code for ShelterDetailView

--- app/src/main/java/com/example/android/homerun/controllers/ShelterDetailView.java
+++ app/src/main/java/com/example/android/homerun/controllers/ShelterDetailView.java
@@ -2,8 +2,14 @@ package com.example.android.homerun.controllers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.android.homerun.R;
+import com.example.android.homerun.model.Shelter;
+
 
 public class ShelterDetailView extends AppCompatActivity {
 
@@ -11,5 +17,39 @@ public class ShelterDetailView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shelter_detail_view);
+
+        Shelter current = (Shelter) getIntent().getSerializableExtra("ShelterData");
+
+        String name = current.getName();
+        AutoCompleteTextView shelter_name_widget = findViewById(R.id.shelter_detail_view_name);
+        shelter_name_widget.setText(name);
+
+        String capacity = current.getCapacity();
+        TextView shelter_capacity_widget = findViewById(R.id.shelter_detail_view_cap);
+        shelter_capacity_widget.setText("Capacity: " + capacity);
+
+        String restrictions = current.getRestrictions();
+        TextView shelter_restrictions_widget = findViewById(R.id.shelter_detail_view_restrictions);
+        shelter_restrictions_widget.setText("Restrictions: " + restrictions);
+
+        String address = current.getAddress();
+        TextView shelter_location_widget = findViewById(R.id.shelter_detail_view_location);
+        shelter_location_widget.setText("Location: " + address);
+
+        String phone_number = current.getPhoneNumber();
+        TextView shelter_phone_widget = findViewById(R.id.shelter_detail_view_phone);
+        shelter_phone_widget.setText("Phone Number: " + phone_number);
+
+        String notes = current.getSpecialNotes();
+        TextView shelter_notes_widget = findViewById(R.id.shelter_detail_view_notes);
+        shelter_notes_widget.setText("Special Notes: " + notes);
+
+        Button reserveButton = findViewById(R.id.shelter_detail_view_reserve);
+        reserveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
